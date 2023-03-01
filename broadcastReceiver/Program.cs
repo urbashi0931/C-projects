a4b70cc9d4516a85f8edf1f229a4431e9eae54bb
urbashi <urbashi0931@gmail.com>
2023-03-01 00:56:00 -0500

    adding socket receiver

---
+++ broadcastReceiver/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace broadcastReceiver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socketBroadcastReceiver = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPEndPoint socketBroadcastReceiverEP = new IPEndPoint(IPAddress.Any, 23000);
+            byte[] receivedBuffer = new byte[512];
+
+            try
+            {
+
+                while (true)
+                {
+                    socketBroadcastReceiver.Bind(socketBroadcastReceiverEP);
+                    int nCountReceived = socketBroadcastReceiver.Receive(receivedBuffer);
+                    Console.WriteLine("no of received bytes count is " + nCountReceived);
+                    Array.Clear(receivedBuffer,0, receivedBuffer.Length);
+                }
+            }
+            catch (Exception Excp)
+            {
+                Console.WriteLine(Excp.ToString());
+            }
+        }
+    }
+}
